3c20e171c870b689269ad6636ae911630f94fefc
Damian (ClassAxion) <damian@techseo.pl>
2021-06-10 16:53:52 +0200

    basic tests

--- tests/parrotDiscoApi.test.ts
+++ tests/parrotDiscoApi.test.ts
@@ -1,9 +1,25 @@
+import { expect } from 'chai';
+
 import ParrotDiscoApi from '../src/parrotDiscoApi';
 
-describe(`Connecting`, () => {
-    it(`Default constructor`, async () => {
-        const drone: ParrotDiscoApi = new ParrotDiscoApi();
+describe(`Basic tests`, () => {
+    let drone: ParrotDiscoApi;
 
-        await drone.connect();
+    it(`Create new instance`, () => {
+        drone = new ParrotDiscoApi();
+    });
+
+    it(`Connect to drone`, async () => {
+        const isConnected = await drone.connect();
+
+        expect(isConnected).equal(true);
+    });
+
+    it(`Should receive any unknown events`, async () => {
+        await new Promise((resolve) => drone.once('unknown', resolve));
+    });
+
+    it(`Move camera`, async () => {
+        await drone.Camera.move(0, 0);
     });
 });
