7b13335423ff3ce8d1f03b4af891178e186f9953
Damian (ClassAxion) <damian@techseo.pl>
2021-06-15 17:06:46 +0200

    camera movement in deg/s

--- src/methods/Camera.method.ts
+++ src/methods/Camera.method.ts
@@ -3,7 +3,11 @@ import ParrotDiscoApi from '../parrotDiscoApi';
 export default class Camera {
     constructor(private instance: ParrotDiscoApi) {}
 
-    public move(tilt: number, pan: number = 0) {
+    public moveTo(tilt: number, pan: number = 0) {
         this.instance.sendCommand([1, 'Camera', 'Orientation', tilt, pan]);
+    }
+
+    public move(tiltDeg: number, panDeg: number = 0) {
+        this.instance.sendCommand([1, 'Camera', 'Velocity', tiltDeg, panDeg]);
     }
 }
